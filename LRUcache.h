3e480ac9df9c78764ca5cc7e652ec3a2e6b827b1
zhanglei1949 <zhanglei1949@sjtu.edu.cn>
2017-10-26 16:21:51 +0100

    add new files

---
+++ LRUcache.h
@@ -0,0 +1,27 @@
+#ifndef LRUCACHE_H_INCLUDED
+#define LRUCACHE_H_INCLUDED
+
+namespace LRUcache
+{
+    //The linked list
+    template<typename key_t, typename value_t>
+    struct Node
+    {
+        key_t key;
+        value_t value;
+
+    };
+
+
+    template<typename key_t, typename value_t>
+    class LRUcache
+    {
+    public:
+
+
+    private:
+
+    };
+}
+
+#endif // LRUCACHE_H_INCLUDED
